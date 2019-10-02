6f2025af44aa0b627cb9ff7e882c940de67f012e
greenorb007 <56039777+greenorb007@users.noreply.github.com>
2019-10-02 13:04:52 +0530

    added std namespace

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 using std::vector;
 
@@ -14,16 +15,16 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
 int main() {
   int n;
   int capacity;
-  std::cin >> n >> capacity;
+  cin >> n >> capacity;
   vector<int> values(n);
   vector<int> weights(n);
   for (int i = 0; i < n; i++) {
-    std::cin >> values[i] >> weights[i];
+    cin >> values[i] >> weights[i];
   }
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  cout.precision(10);
+  cout << optimal_value << endl;
   return 0;
 }
